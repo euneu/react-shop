0dbcb54fed83b999a6220f16f2b2beb5d3bb47d3
hanzii <pwe1ewq@gmail.com>
2022-07-14 00:20:55 +0900

    useEffect, ajax, 애니메이션

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { InputGroup, Form, Button } from "react-bootstrap";
 
 function Detail({ shoes }) {
   let { id } = useParams();
+  const [show, setShow] = useState(true);
+  const [product, setProduct] = useState(0);
   let shoesFind = shoes.find((x) => x.id == id);
-  console.log(shoesFind.id);
+  let [fade, setFade] = useState("");
+
+  const onChange = (e) => {
+    setProduct(e.target.value);
+  };
+
+  //useEffect는 html렌더링 후에 동작
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setShow(false);
+    }, 2000);
+    return () => {
+      //clean up function
+      //이게 먼저 실행되면서 useEffect가 실행되기 전에 먼저 실행됨
+      clearTimeout(timer);
+    };
+  });
+
+  useEffect(() => {
+    if (isNaN(product)) {
+      alert("숫자만 입력하세요");
+    }
+  }, [product]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFade("fadeEnd");
+    }, 10);
+    return () => {
+      setFade("");
+    };
+  }, []);
+
   return (
-    <div className="container">
+    <div className={`container fadeStart ${fade}`}>
+      {show ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
           <img
@@ -17,7 +57,16 @@ function Detail({ shoes }) {
           <h4 className="pt-5">{shoesFind.title}</h4>
           <p>{shoesFind.content}</p>
           <p>{shoesFind.price}</p>
-          <button className="btn btn-danger">주문하기</button>
+          <InputGroup className="mb-3">
+            <Form.Control
+              onChange={onChange}
+              value={product}
+              placeholder="수량을 입력하세요"
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+              주문하기
+            </Button>
+          </InputGroup>
         </div>
       </div>
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,8 +2,12 @@ import data from "../data.js";
 import { Container, Col, Row } from "react-bootstrap";
 import { useState } from "react";
 import Shoes from "../components/Shoes";
+import axios from "axios";
 
 function Home({ shoes }) {
+  let [newShoes, setNewShoses] = useState(shoes);
+  let [click, setClick] = useState(0);
+  const [btnShow, setBtnShow] = useState(true);
   return (
     <div className="App">
       {/* 메인 화면 */}
@@ -11,7 +15,7 @@ function Home({ shoes }) {
       {/* 신발 레이아웃 */}
       <Container>
         <Row>
-          {shoes.map((shoes, index) => (
+          {newShoes.map((shoes, index) => (
             <Shoes
               id={index}
               title={shoes.title}
@@ -21,6 +25,35 @@ function Home({ shoes }) {
           ))}
         </Row>
       </Container>
+      <div>
+        {btnShow ? (
+          <button
+            onClick={() => {
+              if (click == 0) {
+                axios
+                  .get("https://codingapple1.github.io/shop/data2.json")
+                  .then((result) => {
+                    //spread 연산자
+                    let copy = [...newShoes, ...result.data];
+                    setNewShoses(copy);
+                    console.log(copy);
+                  })
+                  .catch(() => {
+                    console.log("전송 실패");
+                  });
+              } else {
+                //상품이 없다는 알림창
+                alert("상품이 없습니다");
+                //버튼 숨겨
+                setBtnShow(false);
+              }
+              setClick((current) => current + 1);
+            }}
+          >
+            추가
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 }
